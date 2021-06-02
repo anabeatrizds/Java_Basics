af058e7ec275def6ea89d3f9d8b262acd0f93054
Ana Beatriz dos Santos <61064552+anabeatrizds@users.noreply.github.com>
2021-06-02 15:50:57 -0300

    Add files via upload

---
+++ Bowling.java
@@ -0,0 +1,61 @@
+/*
+Bowling Game
+
+
+You are creating a bowling game!
+The given code declares a Bowling class with its constructor and addPlayer() method.
+Each player of the game has a name and points, and are stored in the players HashMap.
+The code in main takes 3 players data as input and adds them to the game.
+You need to add a getWinner() method to the class, which calculates and outputs the name of the player with the maximum points.
+
+Sample Input:
+Dave 42
+Amy 103
+Rob 64
+
+Sample Output:
+Amy
+You need to iterate through the HashMap to find the element with the maximum points and output its corresponding key.
+*/
+
+import java.util.*; 
+
+public class Bowling {
+    HashMap<String, Integer> players;
+    Bowling() {
+        players = new HashMap<String, Integer>();
+    }
+    public void addPlayer(String name, int p) {
+        players.put(name, p);
+    }
+    public void getWinner() {
+        String winner = "";
+        int score = 0;
+
+        for (Map.Entry<String, Integer> entry : players.entrySet()) {
+            if (entry.getValue() > score) {
+                score = entry.getValue();
+                winner = entry.getKey();
+            }
+        }
+
+        System.out.println(winner);
+    }
+    
+}
+
+public class Program {
+    public static void main(String[ ] args) {
+        Bowling game = new Bowling();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<3;i++) {
+            String input = sc.nextLine();
+            String[] values = input.split(" ");
+            String name = values[0];
+            int points = Integer.parseInt(values[1]);
+            game.addPlayer(name, points);
+        }
+        game.getWinner();
+    }
+}
